dc4252e250349ffb4a0138cf79bb0b049dda7d35
JrPhy <j0958322080@gmail.com>
2021-07-17 22:49:29 +0800

    Add files via upload

---
+++ single-linked-list.c
@@ -0,0 +1,151 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct _node
+{
+    int data;
+    struct _node *next;
+}node;
+
+node* newNode(int value)
+{
+    node *tmpNode = malloc(sizeof(node));
+    if(tmpNode!=NULL) {
+        tmpNode->data = value;
+        tmpNode->next = NULL;
+    }
+    return tmpNode;
+}
+
+void printList(node *list)
+{
+    while(list != NULL)
+    {
+        printf("%d->", list->data);
+        list = list->next;
+    }
+}
+
+int lenOfList(node *list)
+{
+    int length = 0;
+    while(list != NULL)
+    {
+        ++length;
+        list = list->next;
+    }
+    return length;
+}
+
+void insertNode(node **list, int value, int position)
+{
+    node *temp = *list;
+    int length = lenOfList(temp);
+    
+    /* insert at the beginning*/
+    node *new_node = newNode(value);
+    if(position < 1)
+    {
+        new_node->next = *list;
+        *list = new_node;
+    }
+    /* insert at the beginning*/
+    /* insert at the middle*/
+    else if (position >= length + 1)
+    {
+        
+        node *temp = *list;
+        while(temp->next != NULL) temp = temp->next;
+        temp->next = new_node;
+    }
+    /* insert at the middle*/
+    /* insert at the ending*/
+    else
+    {
+        node *new_node = newNode(value);
+        for(int i = 1; i < position; i++) if(temp->next != NULL) temp = temp->next;
+        new_node->next = temp->next;
+        temp->next = new_node;
+    }
+    /* insert at the ending*/
+}
+
+void deleteNode(node **list, int position) 
+{
+
+    if (*list == NULL) return;
+
+    node *temp = *list;
+    if (position <= 0) 
+    {
+        *list = temp->next;
+        free(temp);
+        return;
+    }
+    else
+    {
+        int length = lenOfList(*list);
+        if (position >= length) position = length - 1;
+    }
+    
+
+    for (int i = 0; temp != NULL && i < position - 1; ++i) temp = temp->next;
+    
+    if (temp == NULL || temp ->next == NULL) return;
+
+    node *nodeToBeDel = temp->next;
+    temp->next = nodeToBeDel->next;
+
+    free(nodeToBeDel);
+}
+
+void reverse(node **list)
+{
+    node *prevNode = NULL, *current = *list, *nextNode = NULL;
+    while (current != NULL) 
+    {
+        nextNode = current->next;
+        current->next = prevNode;
+        prevNode = current;
+        current = nextNode;
+    }
+    *list = prevNode;
+}
+
+int main()
+{
+    /* Initialize nodes */
+    node *one = newNode(1);
+    node *two = newNode(2);
+    node *three = newNode(3);
+    
+    one->next = two;
+    two->next = three;
+    three->next = NULL;
+    // insert new node at the beginning
+    printList(one);
+    printf("\n");
+    insertNode(&one, 4, 0);
+    printList(one);
+    printf("\n");
+    // insert new node at the ending
+    insertNode(&one, 5, 10);
+    printList(one);
+    printf("\n");
+
+    // insert new node in the middle
+    insertNode(&one, 7, 2);
+    printList(one);
+    printf("\n");
+
+    deleteNode(&one, 10);
+    printList(one);
+    printf("\n");
+
+    deleteNode(&one, 3);
+    printList(one);
+    printf("\n");
+    
+    reverse(&one);
+    printList(one);
+    printf("\n");
+}
