813fb759da146818e276b665a8eb5b456ca28178
JrPhy <j0958322080@gmail.com>
2025-10-08 21:05:35 +0800

    Update Tree.c

--- List_and_Tree/Tree.c
+++ List_and_Tree/Tree.c
@@ -1,18 +1,15 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-typedef struct _node
-{
+typedef struct _node {
     int data;
     struct _node *left;
     struct _node *right;
 }node;
 
-node* newNode(int value)
-{
+node* newNode(int value) {
     node *tmpNode = (node*) malloc(sizeof(node));
-    if(tmpNode)
-    {
+    if(tmpNode) {
         tmpNode->data = value;
         tmpNode->left = NULL;
         tmpNode->right = NULL;
@@ -20,21 +17,17 @@ node* newNode(int value)
     return tmpNode;
 }
 
-void inorder(node *root)
-{
+void inorder(node *root) {
     printf("%d, ", root->data);
     if (root->left)  //若其中一側的子樹非空則會讀取其子樹
         inorder(root->left);
     if (root->right) //另一側的子樹也做相同事
         inorder(root->right);
-
 }
 
-node *search(node *root, int key)
-{
+node *search(node *root, int key) {
     node *current = root;
-    while(current && key != current->data)
-    {
+    while(current && key != current->data) {
         if(key > current->data) current = current->right; //若比較大則往右走
         else current = current->left;                 //否則往左走
     }
@@ -42,11 +35,9 @@ node *search(node *root, int key)
     else return NULL;
 }
 
-void insert(node **root, int value)
-{
+void insert(node **root, int value) {
     node *current = *root, *prevNode = NULL;
-    while(current)
-    {
+    while(current) {
         prevNode = current;
         if(prevNode->data > value && value > current->data) break;
         if(current->data > value && value > prevNode->data) break;
@@ -56,22 +47,19 @@ void insert(node **root, int value)
     }
 
     if(!prevNode) prevNode = newNode(value);
-    else if (value < prevNode->data)
-    {
+    else if (value < prevNode->data) {
         prevNode->left = newNode(value);
         prevNode = prevNode->left;
         prevNode->left = current;
     }
-    else
-    {
+    else {
         prevNode->right = newNode(value);
         prevNode = prevNode->right;
         prevNode->right = current;
     }
 }
 
-void deleteNode(node **root, int value)
-{
+void deleteNode(node **root, int value) {
     node *current = *root, *prevNode = NULL;
     while(current->data != value)
     {
@@ -85,17 +73,14 @@ void deleteNode(node **root, int value)
     }
     printf("\n%d\n", current->data);
 
-    if(!current->left && !current->right)
-    {
+    if(!current->left && !current->right) {
         if (prevNode->left == current)  prevNode->left = NULL;
         else prevNode->right = NULL;
         free(current);
     }
-    else if(current->left && current->right)
-    {
+    else if(current->left && current->right) {
         node *tempNode = current->right, *prevTemp = NULL;
-        while(tempNode->left)
-        {
+        while(tempNode->left) {
             prevTemp = tempNode;
             tempNode = tempNode->left;
         }
@@ -104,30 +89,23 @@ void deleteNode(node **root, int value)
         current->data = tempNode->data;
         free(tempNode);
     }
-    else
-    {
-        if (!prevNode)
-        {
-            if(current->right) 
-            {
+    else {
+        if (!prevNode) {
+            if(current->right) {
                 (*root)->data = current->right->data;
                 (*root)->right = NULL;
             }
-            else 
-            {
+            else {
                 (*root)->data = current->left->data;
                 (*root)->left = NULL;
             }
         }
-        else
-        {
-            if (prevNode->data > current->data)
-            {
+        else {
+            if (prevNode->data > current->data) {
                 if (current->left) prevNode->left = current->left;
                 else prevNode->left = current->right;
             }
-            else
-            {
+            else {
                 if (current->left) prevNode->right = current->left;
                 else prevNode->right = current->right;
             }
@@ -136,8 +114,7 @@ void deleteNode(node **root, int value)
     }
 }
 
-int main()
-{
+int main() {
     /* Initialize nodes */
     node *root = newNode(11);
     insert(&root, 5);
@@ -157,3 +134,4 @@ int main()
     inorder(root);
     return 0;
 }
+
